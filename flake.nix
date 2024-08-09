b6731fcf649d82b0e5edee9335b55710d7887fef
Noam Yorav-Raphael <noamraph@gmail.com>
2024-08-09 08:29:17 +0300

    'nix build' working to build the nixsa static executable!

---
+++ flake.nix
@@ -0,0 +1,206 @@
+# This is based on the nix-installer flake.nix.
+# I'm no Nix expert, please let me know how this could be improved.
+{
+  description = "The Nixsa standalone nix tarball";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.0.tar.gz";
+
+    fenix = {
+      url = "https://flakehub.com/f/nix-community/fenix/0.1.1584.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix = {
+      url = "github:noamraph/nix/add-nix-state-home";
+      # url = "https://flakehub.com/f/DeterminateSystems/nix/=2.23.3.tar.gz";
+    };
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , fenix
+    , naersk
+    , nix
+    , ...
+    } @ inputs:
+    let
+      supportedSystems = [ "i686-linux" "x86_64-linux" "aarch64-linux" ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: (forSystem system f));
+
+      forSystem = system: f: f rec {
+        inherit system;
+        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
+        lib = pkgs.lib;
+      };
+
+      fenixToolchain = system: with fenix.packages.${system};
+        combine ([
+          stable.clippy
+          stable.rustc
+          stable.cargo
+          stable.rustfmt
+          stable.rust-src
+        ] ++ nixpkgs.lib.optionals (system == "x86_64-linux") [
+          targets.x86_64-unknown-linux-musl.stable.rust-std
+        ] ++ nixpkgs.lib.optionals (system == "i686-linux") [
+          targets.i686-unknown-linux-musl.stable.rust-std
+        ] ++ nixpkgs.lib.optionals (system == "aarch64-linux") [
+          targets.aarch64-unknown-linux-musl.stable.rust-std
+        ]);
+
+    in
+    {
+      overlays.default = final: prev:
+        let
+          toolchain = fenixToolchain final.stdenv.system;
+          naerskLib = final.callPackage naersk {
+            cargo = toolchain;
+            rustc = toolchain;
+          };
+          sharedAttrs = {
+            pname = "nixsa";
+            version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+            src = builtins.path {
+              name = "nixsa-source";
+              path = self;
+              filter = (path: type: baseNameOf path != "nix" && baseNameOf path != ".github");
+            };
+
+            nativeBuildInputs = with final; [ ];
+
+            doCheck = true;
+            cargoTestOptions = f: f ++ [ "--all" ];
+
+            override = { preBuild ? "", ... }: {
+              preBuild = preBuild + ''
+                # logRun "cargo clippy --all-targets --all-features -- -D warnings"
+              '';
+            };
+          };
+        in
+        rec {
+          nixsa = naerskLib.buildPackage sharedAttrs;
+        } // nixpkgs.lib.optionalAttrs (prev.stdenv.system == "x86_64-linux") rec {
+          default = nixsa-static;
+          nixsa-static = naerskLib.buildPackage
+            (sharedAttrs // {
+              CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+            });
+        } // nixpkgs.lib.optionalAttrs (prev.stdenv.system == "i686-linux") rec {
+          default = nixsa-static;
+          nixsa-static = naerskLib.buildPackage
+            (sharedAttrs // {
+              CARGO_BUILD_TARGET = "i686-unknown-linux-musl";
+            });
+        } // nixpkgs.lib.optionalAttrs (prev.stdenv.system == "aarch64-linux") rec {
+          default = nixsa-static;
+          nixsa-static = naerskLib.buildPackage
+            (sharedAttrs // {
+              CARGO_BUILD_TARGET = "aarch64-unknown-linux-musl";
+            });
+        };
+
+
+      devShells = forAllSystems ({ system, pkgs, ... }:
+        let
+          toolchain = fenixToolchain system;
+          # check = import ./nix/check.nix { inherit pkgs toolchain; };
+        in
+        {
+          default = pkgs.mkShell {
+            name = "nixsa-shell";
+
+            RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+
+            nativeBuildInputs = with pkgs; [ ];
+            buildInputs = with pkgs; [
+              toolchain
+              shellcheck
+              rust-analyzer
+              cargo-outdated
+              cacert
+              cargo-audit
+              cargo-watch
+              nixpkgs-fmt
+              check.check-rustfmt
+              check.check-spelling
+              check.check-nixpkgs-fmt
+              check.check-editorconfig
+              check.check-semver
+              check.check-clippy
+            ]
+            ++ lib.optionals (pkgs.stdenv.isLinux) (with pkgs; [
+              checkpolicy
+              semodule-utils
+              /* users are expected to have a system docker, too */
+            ]);
+          };
+        });
+
+      checks = forAllSystems ({ system, pkgs, ... }:
+        let
+          toolchain = fenixToolchain system;
+          check = import ./nix/check.nix { inherit pkgs toolchain; };
+        in
+        {
+          check-rustfmt = pkgs.runCommand "check-rustfmt" { buildInputs = [ check.check-rustfmt ]; } ''
+            cd ${./.}
+            check-rustfmt
+            touch $out
+          '';
+          check-spelling = pkgs.runCommand "check-spelling" { buildInputs = [ check.check-spelling ]; } ''
+            cd ${./.}
+            check-spelling
+            touch $out
+          '';
+          check-nixpkgs-fmt = pkgs.runCommand "check-nixpkgs-fmt" { buildInputs = [ check.check-nixpkgs-fmt ]; } ''
+            cd ${./.}
+            check-nixpkgs-fmt
+            touch $out
+          '';
+          check-editorconfig = pkgs.runCommand "check-editorconfig" { buildInputs = [ pkgs.git check.check-editorconfig ]; } ''
+            cd ${./.}
+            check-editorconfig
+            touch $out
+          '';
+        });
+
+      packages = forAllSystems ({ system, pkgs, ... }:
+        {
+          inherit (pkgs) nixsa;
+        } // nixpkgs.lib.optionalAttrs (system == "x86_64-linux") {
+          inherit (pkgs) nixsa-static;
+          default = pkgs.nixsa-static;
+        } // nixpkgs.lib.optionalAttrs (system == "i686-linux") {
+          inherit (pkgs) nixsa-static;
+          default = pkgs.nixsa-static;
+        } // nixpkgs.lib.optionalAttrs (system == "aarch64-linux") {
+          inherit (pkgs) nixsa-static;
+          default = pkgs.nixsa-static;
+        } // nixpkgs.lib.optionalAttrs (pkgs.stdenv.isDarwin) {
+          default = pkgs.nixsa;
+        });
+
+      # hydraJobs = {
+      #   vm-test = import ./nix/tests/vm-test {
+      #     inherit forSystem;
+      #     inherit (nixpkgs) lib;
+
+      #     binaryTarball = nix.tarballs_indirect;
+      #   };
+      #   container-test = import ./nix/tests/container-test {
+      #     inherit forSystem;
+
+      #     binaryTarball = nix.tarballs_indirect;
+      #   };
+      # };
+    };
+}
---
+++ nix/check.nix
@@ -0,0 +1,63 @@
+# Based on nix/check.nix from nix-installer
+{ pkgs, toolchain }:
+
+let
+  inherit (pkgs) writeShellApplication;
+in
+{
+
+  # Format
+  check-rustfmt = (writeShellApplication {
+    name = "check-rustfmt";
+    runtimeInputs = [ toolchain ];
+    text = "cargo fmt --check";
+  });
+
+  # Spelling
+  check-spelling = (writeShellApplication {
+    name = "check-spelling";
+    runtimeInputs = with pkgs; [ git codespell ];
+    text = ''
+      codespell \
+        --ignore-words-list="ba,sur,crate,pullrequest,pullrequests,ser,distroname" \
+        --skip="./target,.git,./src/action/linux/selinux" \
+        .
+    '';
+  });
+
+  # NixFormatting
+  check-nixpkgs-fmt = (writeShellApplication {
+    name = "check-nixpkgs-fmt";
+    runtimeInputs = with pkgs; [ git nixpkgs-fmt findutils ];
+    text = ''
+      nixpkgs-fmt --check .
+    '';
+  });
+
+  # EditorConfig
+  check-editorconfig = (writeShellApplication {
+    name = "check-editorconfig";
+    runtimeInputs = with pkgs; [ eclint ];
+    text = ''
+      eclint .
+    '';
+  });
+
+  # Semver
+  check-semver = (writeShellApplication {
+    name = "check-semver";
+    runtimeInputs = with pkgs; [ cargo-semver-checks ];
+    text = ''
+      cargo-semver-checks semver-checks check-release
+    '';
+  });
+  # Clippy
+  check-clippy = (writeShellApplication {
+    name = "check-clippy";
+    runtimeInputs = with pkgs; [ cargo clippy rustc ];
+    text = ''
+      cargo clippy
+    '';
+  });
+
+}
